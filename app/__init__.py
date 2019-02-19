7953672e8e6b70329e55f59969b321a39bba6331
Gonzalo <gonzalochica@uma.es>
2019-02-19 12:33:54 +0100

    User Registration and Login

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+import logging
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+bootstrap = Bootstrap(app)
+mail = Mail(app)
+
+if not app.debug:
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/5genesis.log', maxBytes=1024000,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('5Genesis startup')
+
+from app import routes, models, errors
