1b1a52cfd3066ef26c96c172de7246241aa622c8
Gonzalo <gonzalochicamorales@lcc.uma.es>
2019-02-26 12:06:11 +0100

    Remove unused parameters in Dispatcher

--- REST/dispatcher_api.py
+++ REST/dispatcher_api.py
@@ -9,7 +9,7 @@ class Dispatcher_Api(RestClient):
     def __init__(self, api_host, api_port, suffix):
         super().__init__(api_host, api_port, suffix)
 
-    def Post(self, experiment_id, experiment_name, user_id):
+    def Post(self, experiment_id):
         url = f'{self.api_url}/run'
         response = self.HttpPost(url, {'Content-Type': 'application/json'},
                                  json.dumps(Experiment.query.get(experiment_id).serialization()))
