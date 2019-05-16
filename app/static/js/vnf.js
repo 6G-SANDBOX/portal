054aa5483f4f5136834097dabe2e85cc8659e405
Gonzalo <gonzalochicamorales@lcc.uma.es>
2019-05-16 15:13:08 +0200

    Refactor VNF JavaScript

--- app/static/js/vnf.js
+++ app/static/js/vnf.js
@@ -1,59 +1,20 @@
-document.querySelector("html").classList.add('js');
+function configureButton(input, trigger, ret)
+{
+    var fileInput = document.querySelector( input ),
+        button = document.querySelector( trigger ),
+        the_return = document.querySelector( ret);
 
-var fileInput1  = document.querySelector( ".input-file1" ),
-    button1     = document.querySelector( ".input-file-trigger1" ),
-    the_return1 = document.querySelector( ".file-return1" );
+    button.addEventListener( "click", function(event) {
+        fileInput.focus();
+        return false;
+    });
 
-button1.addEventListener( "click", function( event ) {
-   fileInput1.focus();
-   return false;
-});
+    fileInput.addEventListener("change", function( event ) {
+        the_return.innerHTML = this.value.split("\\")[2];
+    });
+}
 
-fileInput1.addEventListener( "change", function( event ) {
-    the_return1.innerHTML = this.value.split("\\")[2];
-});
-
-///////////////
-
-var fileInput2  = document.querySelector( ".input-file2" ),
-    button2     = document.querySelector( ".input-file-trigger2" ),
-    the_return2 = document.querySelector( ".file-return2" );
-
-button2.addEventListener( "click", function( event ) {
-   fileInput2.focus();
-   return false;
-});
-
-fileInput2.addEventListener( "change", function( event ) {
-    the_return2.innerHTML = this.value.split("\\")[2];
-});
-
-///////////////
-
-var fileInput3  = document.querySelector( ".input-file3" ),
-    button3     = document.querySelector( ".input-file-trigger3" ),
-    the_return3 = document.querySelector( ".file-return3" );
-
-button3.addEventListener( "click", function( event ) {
-   fileInput3.focus();
-   return false;
-});
-
-fileInput3.addEventListener( "change", function( event ) {
-    the_return3.innerHTML = this.value.split("\\")[2];
-});
-
-///////////////
-
-var fileInput4  = document.querySelector( ".input-file4" ),
-    button4     = document.querySelector( ".input-file-trigger4" ),
-    the_return4 = document.querySelector( ".file-return4" );
-
-button4.addEventListener( "click", function( event ) {
-   fileInput4.focus();
-   return false;
-});
-
-fileInput4.addEventListener( "change", function( event ) {
-    the_return4.innerHTML = this.value.split("\\")[2];
-});
+configureButton(".input-file1", ".input-file-trigger1", ".file-return1");
+configureButton(".input-file2", ".input-file-trigger2", ".file-return2");
+configureButton(".input-file3", ".input-file-trigger3", ".file-return3");
+configureButton(".input-file4", ".input-file-trigger4", ".file-return4");
