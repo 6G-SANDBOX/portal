a90f9d484245f5af92d45a5f005493f0e5b7cd9f
Gonzalo <gonzalochicamorales@lcc.uma.es>
2019-05-28 12:32:01 +0200

    Add ID to execution status

--- app/api/routes.py
+++ app/api/routes.py
@@ -10,19 +10,21 @@ import json
 def set_execution_status(execution_id):
     data = json.loads(request.data.decode('utf8'))
     execution = Execution.query.get(int(execution_id))
-    if 'Status' in data:
-        if data["Status"] in ['PreRun']:
-            execution.start_time = datetime.utcnow()
-        elif data["Status"] in ['Finished', 'Cancelled', 'Errored']:
-            execution.end_time = datetime.utcnow()
-        execution.status = data["Status"]
-    if 'Dashboard' in data:
-        execution.dashboard_url = str(data["Dashboard"])
-    if 'PerCent' in data:
-        execution.percent = data["PerCent"]
-    if 'Message' in data:
-        execution.message = str(data["Message"])
-    db.session.commit()
-
-    print('Status of Execution ' + str(execution.id) + ' changed to ' + execution.status)
+    if execution:
+        if 'Status' in data:
+            if data["Status"] in ['PreRun']:
+                execution.start_time = datetime.utcnow()
+            elif data["Status"] in ['Finished', 'Cancelled', 'Errored']:
+                execution.end_time = datetime.utcnow()
+            execution.status = data["Status"]
+            print('Status of Execution ' + str(execution.id) + ' changed to ' + execution.status)
+        if 'Dashboard' in data:
+            execution.dashboard_url = str(data["Dashboard"])
+        if 'PerCent' in data:
+            execution.percent = data["PerCent"]
+            print('PerCent ' + str(execution.percent))
+        if 'Message' in data:
+            execution.message = str(data["Message"])
+            print('Message ' + str(execution.message))
+        db.session.commit()
     return ""
